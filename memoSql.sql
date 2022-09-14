a407a55570e05bbaaf6ba7570f1c08cb5fcbdaa8
disanv03 <disanv03@gmail.com>
2022-09-14 15:26:30 +0200

    memo UPDATE

--- memoSql.sql
+++ memoSql.sql
@@ -118,3 +118,66 @@ INSERT INTO recipes (
 -- CREATE UNIQUE INDEX idx_slug ON recipes (slug);
 
 -- UPDATE recipes SET slug = 'soupe2' WHERE id = 4;
+
+-- EXPLAIN QUERY PLAN SELECT * FROM recipes WHERE slug = 'soupe';
+
+-- PRAGMA index_list('recipes');
+
+-- DROP INDEX idx_slug;
+/*
+ CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    slug VARCHAR(150) NOT NULL UNIQUE,
+    content TEXT DEFAULT 'hello' NOT NULL,
+    duration SMALLINT DEFAULT 10 NOT NULL,
+    online BOOLEAN,
+    created_at DATETIME
+);*/
+
+/*INSERT INTO recipes(
+    title,
+    slug,
+    online,
+    created_at
+    ) VALUES (
+    'Soupe1',
+    'soupe-2',
+    FALSE,
+    1642696734
+);*/
+
+-- SELECT * FROM recipes WHERE content IS NULL;
+-- SELECT * FROM recipes WHERE content IS NOT NULL;
+
+-- UPDATE recipes SET content = NULL;
+-- DROP TABLE recipes
+
+
+-- CLES ETRANGERES ET JOINTURES
+
+CREATE TABLE IF NOT EXISTS categories(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    description TEXT
+);
+
+INSERT INTO categories(title)
+VALUES('Plat'),('Dessert');
+
+CREATE TABLE recipes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    slug VARCHAR(50) NOT NULL UNIQUE,
+    content TEXT,
+    category_id INTEGER, 
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+); 
+
+INSERT INTO recipes (title, slug, category_id)
+    VALUES
+    ('Creme Anglaise', 'creme-anglaise', 2),
+    ('Soupe', 'soupe', 1),
+    ('Salade de fruit', 'salade-de-fruit', 2);
+
+
